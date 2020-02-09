cba83431c0f2636403676f6622fdc5f0a39cd292
C4LLZ <weggethegamer@gmail.com>
2020-02-09 19:05:58 +0100

    Upload
    +Added files

---
+++ main.js
@@ -0,0 +1,23 @@
+
+    setInterval(function(){
+        
+        var box = document.getElementsByClassName('claimable-bonus__icon');
+        let url = chrome.runtime.getURL('Pling.mp3')
+    if (box.length > 0) {
+      document.querySelector('.claimable-bonus__icon').click();
+      console.log("A box was redeemed!");
+      let a = new Audio(url)
+      a.play()
+    }else{
+            console.log("There was no redeemable box. Rechecking in 10 seconds!");
+            
+        };  
+    }, 10000);
+    
+      
+
+ 
+
+    
+
+
