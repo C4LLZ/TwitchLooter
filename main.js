9a7c8dd8b2f91f877f2adb8306a64fd14e8fa3fb
C4LLZ <weggethegamer@gmail.com>
2020-02-22 15:58:35 +0100

    Beta

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
+var num = (JSON.parse(localStorage.getItem("Redeemed")));
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var link = document.getElementById('clear');
+  Looted.innerHTML = JSON.parse(localStorage.getItem("Redeemed"));
+  // onClick's logic below:
+  link.addEventListener('click', function() {
+      localStorage.clear();
+    console.log("tried clearing.")
+    });
+})
+
+    // Lotting script. 
 
     setInterval(function(){
         
       var box = document.getElementsByClassName('claimable-bonus__icon');
+      console.log(num)
   if (box.length > 0) {
     document.querySelector('.claimable-bonus__icon').click();
     console.log("A box was redeemed!");
+    num += 1;
+    localStorage.setItem("Redeemed", num)
 
   }else{
           console.log("There was no redeemable box. Rechecking in 10 seconds!");
-          
+          num += 1;
+          localStorage.setItem("Redeemed", num)
       };  
   }, 10000);
+
+
+
+
+
