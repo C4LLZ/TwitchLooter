3d622fb360cdfb9aad3e7af77c556f76c5340a1b
C4LLZ <33510329+C4LLZ@users.noreply.github.com>
2020-12-20 18:02:46 +0100

    Added Notifications

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 
-    setInterval(function(){
-        
-      var box = document.getElementsByClassName('claimable-bonus__icon');
-  if (box.length > 0) {
-    document.querySelector('.claimable-bonus__icon').click();
-    console.log("A box was redeemed!");
 
-  }else{
-          console.log("There was no redeemable box. Rechecking in 10 seconds!");
-          
-      };  
-  }, 10000);
+setInterval(function(){
+   
+var box = document.getElementsByClassName('claimable-bonus__icon');
+if (box.length > 0) {
+document.querySelector('.claimable-bonus__icon').click();
+console.log("A box was redeemed!");
+$.notify("Box Redeemed","success");
+
+}else{
+        console.log("There was no redeemable box. Rechecking in 10 seconds!");
+         
+     };  
+}, 10000);
+
+
